57c36623a09b4f758659a1e933608138a5aa31df
Levi Schwartzberg <Levischwartzberg@gmail.com>
2021-05-14 14:05:10 -0500

    fixed more packagejson garbage

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import React from "react";
 import Timer from "./components/Timer";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch,
-  Redirect,
-  useLocation
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Link,
+//   Switch,
+//   Redirect,
+//   useLocation
+// } from "react-router-dom";
 
 function App() {
   return ( 
--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 
-import './styles.css';
+// import './styles.css';
 
 function Timer() {
   const [counter, setCounter] = React.useState(60);
