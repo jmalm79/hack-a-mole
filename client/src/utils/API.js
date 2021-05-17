63225922a87fa990e99fd0a49830bf01294aa947
Drew Olsen <olsenj.andrew@gmail.com>
2021-05-17 14:42:10 -0500

    Delete API.js
    Accidentally pushed

--- client/src/utils/API.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-import axios from "axios";
-
-export default {
-  // Gets all phrases/insults
-  getInsults: function() {
-    return axios.get("/api/phrases")
-  }
-};
