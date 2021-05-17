b74fd7e0296c0007ee6fd393d32389f38be95ca8
Levi Schwartzberg <Levischwartzberg@gmail.com>
2021-05-17 12:07:46 -0500

    added a second mole to the equation

---
+++ client/src/components/Mole2/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Mole1(props) {
+    return(
+        <div className="mole2" id={props.id} onClick={props.handleClick2}>
+
+        </div>
+    )
+}
+
+export default Mole1;
