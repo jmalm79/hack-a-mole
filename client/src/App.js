94931a00cf1c03da4b9f8dfdb197d444ded8e546
Drew Olsen <olsenj.andrew@gmail.com>
2021-05-17 12:38:52 -0500

    Header and footer styles about done

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import React, { useState, useEffect } from "react";
-import Computer from "./components/Computer";
-import GameGrid from "./components/GameGrid";
-import Timer from './components/Timer';
-import StartBtn from './components/StartBtn';
+import React, { useState, useEffect } from 'react';
+import Share from './components/Share';
+import GameGrid from './components/GameGrid';
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
-import Share from './components/Share';
+import Game from './components/layout/Game';
+import HighScores from './components/layout/HighScores';
+import Instructions from './components/layout/Instructions';
 // import {
 //   BrowserRouter as Router,
 //   Route,
@@ -17,43 +17,44 @@ import Share from './components/Share';
 // } from "react-router-dom";
 
 function App() {
-  // const [timer, setTimer ] = useState(10);
-  // const [playing, setPlaying ] = useState(false);
+	// const [timer, setTimer ] = useState(10);
+	// const [playing, setPlaying ] = useState(false);
 
-  // const handleStartBtn = event => {
-  //   if (playing === false) {
-  //     setTimer(10);
-  //     setPlaying(true);
-  //     setTimeout(() => setTimer(timer-1), 1000)
-  //   }
-  // }
+	// const handleStartBtn = event => {
+	//   if (playing === false) {
+	//     setTimer(10);
+	//     setPlaying(true);
+	//     setTimeout(() => setTimer(timer-1), 1000)
+	//   }
+	// }
 
-  // const handleClick1 = event => {
-  //   console.log(event.target);
-  // }
+	// const handleClick1 = event => {
+	//   console.log(event.target);
+	// }
 
-  // useEffect(() => {
-  //   if (timer > -1 && playing === true) {
-  //     setTimeout(() => setTimer(timer-1), 1000)
-  //   }
-  //   else {
-  //     setTimer(10);
-  //     //other end game logic, reactions here
-  //     setPlaying(false);
-  //   }
-  //   }, [timer])
+	// useEffect(() => {
+	//   if (timer > -1 && playing === true) {
+	//     setTimeout(() => setTimer(timer-1), 1000)
+	//   }
+	//   else {
+	//     setTimer(10);
+	//     //other end game logic, reactions here
+	//     setPlaying(false);
+	//   }
+	//   }, [timer])
 
-  return ( 
-     <div>
-        <Header />
-        <Share />
-        <p>Test :)</p>
-        <GameGrid/>
-        <Footer />
-      </div>
-  )
-
+	return (
+		<div>
+			<Header />
+			<Share />
+			<p>Test :)</p>
+			<GameGrid />
+			<Footer />
+			<Game />
+			<HighScores />
+			<Instructions />
+		</div>
+	);
 }
-
 
 export default App;
