3269d3f9deeaa7e3c62f86ffe94e811b43fe39f0
Drew Olsen <olsenj.andrew@gmail.com>
2021-05-19 14:34:08 -0500

    Styled form in the game over component

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -2,49 +2,53 @@ import React, { useState } from 'react';
 import API from '../../utils/API';
 
 function AddHighScore(props) {
-    
-    const [formObject, setFormObject] = useState({})
+	const [
+		formObject,
+		setFormObject
+	] = useState({});
 
-    function handleInputChange(event) {
-        const { name, value } = event.target;
-        setFormObject({...formObject, [name]: value})
-      };
+	function handleInputChange(event) {
+		const { name, value } = event.target;
+		setFormObject({ ...formObject, [name]: value });
+	}
 
-    function handleFormSubmit(event) {
-        event.preventDefault();
-        if (formObject.name && props.score) {
-          API.newHighscore({
-            name: formObject.name,
-            score: props.score,
-          })
-          .catch(err => console.log(err));
-        }
-      };
+	function handleFormSubmit(event) {
+		event.preventDefault();
+		if (formObject.name && props.score) {
+			API.newHighscore({
+				name  : formObject.name,
+				score : props.score
+			}).catch((err) => console.log(err));
+		}
+	}
 
-      function PlayAgain() {
-        props.PlayAgain();
-      }
-    
-    return (
-      <form>
-        <div className="field">
-          <label htmlFor="name">Name</label>
-          <input
-            onChange={handleInputChange}
-            type="text"
-            name="name"
-          />
-        </div>
-        <label htmlFor="score">Score</label>{props.score}
-        <button type="submit"
-        disabled={!(formObject.name && props.score)}
-        onClick={() => {
-          handleFormSubmit();
-          PlayAgain();
-        }}
-        >Save</button>
-      </form>
-    );
-  };
+	function PlayAgain() {
+		props.PlayAgain();
+	}
+
+	return (
+		<form>
+			<h3>
+				Score: <span className={'score'}>{props.score}</span>
+			</h3>
+			<label className={'label'} htmlFor="name">
+				Name <br />
+				<input onChange={handleInputChange} placeholder="Name" type="text" name="name" />
+			</label>
+
+			<button
+				className={'btn submit'}
+				type="submit"
+				disabled={!(formObject.name && props.score)}
+				onClick={() => {
+					handleFormSubmit();
+					PlayAgain();
+				}}
+			>
+				Save
+			</button>
+		</form>
+	);
+}
 
 export default AddHighScore;
